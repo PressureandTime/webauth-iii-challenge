7288acb86da90cbd3a6530035ea3f3ccfd7f7fbb
Petar Scepanovic <hiborija@gmail.com>
2019-07-24 16:25:23 +0200

    finished the project

--- auth/check-role-middleware.js
+++ auth/check-role-middleware.js
@@ -2,6 +2,6 @@ module.exports = role => function(req, res, next) {
   if (req.decodedToken.roles && req.decodedToken.roles.includes(role)) {
     next();
   } else {
-    res.status(403).json({ you: 'HAVE NO POWER HERE!' });
+    res.status(403).json({ you: 'you shall not pass!' });
   }
 };
